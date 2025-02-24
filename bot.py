b9431dce8b9ca96e8a5befb7fd2d6ddba17cd943
Parth Sarthi Mishra <90957276+mrsarthi@users.noreply.github.com>
2025-02-24 18:25:36 +0530

    added google form support

--- bot.py
+++ bot.py
@@ -12,18 +12,27 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 # Open a sample form (replace with any actual form URL)
-driver.get("https://www.w3schools.com/html/html_forms.asp")
+driver.get("https://docs.google.com/forms/d/e/1FAIpQLScxfFwR8U2buJ8nwGQdoJmUi0--YL8bxkGMUbk8lrREbTgx2g/viewform?usp=header")
 
 # Fill out the form (adjust element names accordingly)
-name_field = driver.find_element(By.NAME, "fname")
+name_field = driver.find_element(By.NAME, "entry.2005620554")
 name_field.send_keys("John Doe")
 
-email_field = driver.find_element(By.NAME, "lname")
+email_field = driver.find_element(By.NAME, "entry.1045781291")
 email_field.send_keys("johndoe@example.com")
 
-# Submit the form (if applicable)
-email_field.send_keys(Keys.RETURN)
+email_field = driver.find_element(By.NAME, "entry.1166974658")
+email_field.send_keys("johndoe@example.com")
+
+email_field = driver.find_element(By.NAME, "entry.839337160")
+email_field.send_keys("johndoe@example.com")
+
+submit_button = driver.find_element(By.XPATH, "//span[text()='Submit']")
+submit_button.click()
+
 
 # Wait for 3 seconds before closing
-time.sleep(3)
+# time.sleep(3)
+input("Press Enter to close the browser...")
+
 driver.quit()
